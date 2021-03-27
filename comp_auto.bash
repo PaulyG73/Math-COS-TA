b525be63264fd8fb669bd3103a3a34911237beec
Paul Guertin <atomman73@hotmail.com>
2021-03-27 13:47:53 -0500

    updated license for MIT

--- comp_auto.bash
+++ comp_auto.bash
@@ -1,7 +1,29 @@
+# MIT License
+
+# Copyright (c) 2021 Paul Guertin
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # comp_auto.bash
-# This script searches for .cpp files and compiles them to run on the linux subsystem.
+# This bash script searches for .cpp files and compiles them to run on the linux subsystem.
 # It then runs each executable just compiled and stores the output in a text file.
-# Last modified 2/26/2021 by Paul Guertin
+
 
 
 OUTPUT=output_execute.txt           # text file where output is stored
